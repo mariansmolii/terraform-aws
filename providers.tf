f01259189224d99b811aa13183f6cef5666bb0ce
Marian Smolii <smoliimarian@gmail.com>
2025-01-29 22:59:08 +0200

    feat: add tls and local to providers

--- providers.tf
+++ providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.84.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
   }
 }
 
